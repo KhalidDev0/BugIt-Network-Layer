d655a337de6ea0a8103271aeca188d16362eeb0f
khaliddev00 <kalmuqrin.c@stc.com.sa>
2023-10-21 15:29:47 +0300

    changed UploadBugToGoogleSheetUseCase return type

--- network-layer/src/main/java/app/bugit/networklayer/domain/googleSheet/useCase/UploadBugToGoogleSheetUseCase.kt
+++ network-layer/src/main/java/app/bugit/networklayer/domain/googleSheet/useCase/UploadBugToGoogleSheetUseCase.kt
@@ -4,13 +4,25 @@ import app.bugit.networklayer.data.model.BugUploadDto
 import app.bugit.networklayer.data.model.Resource
 import app.bugit.networklayer.domain.googleSheet.repository.GoogleSheetRepository
 import kotlinx.coroutines.flow.Flow
-import retrofit2.Response
+import kotlinx.coroutines.flow.flow
 
 class UploadBugToGoogleSheetUseCase(
     private val googleSheetRepository: GoogleSheetRepository
 ) {
 
-    operator fun invoke(title: String, description: String, imageUrl: String): Flow<Resource<Response<BugUploadDto>>> {
-        return googleSheetRepository.uploadBugToGoogleSheet(title, description, imageUrl)
+    operator fun invoke(title: String, description: String, imageUrl: String): Flow<Resource<BugUploadDto>> = flow {
+        googleSheetRepository.uploadBugToGoogleSheet(title, description, imageUrl).collect { resource ->
+            when (resource) {
+                is Resource.Loading -> {
+                    emit(Resource.Loading())
+                }
+                is Resource.Success -> {
+                    emit(Resource.Success(resource.data.body()!!))
+                }
+                is Resource.Error -> {
+                    emit(Resource.Error(resource.error))
+                }
+            }
+        }
     }
 }
